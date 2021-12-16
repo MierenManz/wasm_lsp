3c0e7392367f376ae9fc41071a7fe74062841639
MierenManz <christiaanmens@gmail.com>
2021-12-16 20:00:16 +0100

    add stuff

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ mod wasm_lexer;
 
 use logos::Logos;
 use wasm_lexer::Tokens;
+
 fn main() {
-    let s = std::fs::read_to_string("./test/test1.wat").unwrap();
+    let s = std::fs::read_to_string("./test/named_ptrs.wat").unwrap();
     let start = std::time::Instant::now();
-    let mut lexer = Tokens::lexer(&s);
+    let lexer = Tokens::lexer(&s);
     let end = start.elapsed().as_nanos();
     for token in lexer {
         println!("{:?}", token)
